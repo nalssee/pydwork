e55948bfd192cac309da9d7b29de405f8936c4f5
nalssee <kenjin@sdf.org>
2016-05-20 17:47:48 +0900

    ..

--- build/lib/pydwork/mypy.py
+++
@@ -1,9 +0,0 @@
-import contextlib, time
-
-@contextlib.contextmanager
-def timeit():
-    start = time.time()
-    try:
-        yield
-    finally:
-        print('\nTotal time: ', time.time() - start)
