d3bab6bcfbfb6717d96febcfde0437f0ac173d6a
nalssee <kenjin@sdf.org>
2017-04-07 21:45:07 +0900

    ..

--- pydwork/fin.py
+++ pydwork/fin.py
@@ -114,7 +114,8 @@ class PRows(Rows):
         for rs1 in newrs.group(self.dcol):
             for pns, rs2 in zip(product(*(range(1, n + 1) for n in ns)),
                                 rs1.order(pncols).group(pncols)):
-                # test if there's any missing portfolio
+                print(rs2, pncols)
+                # test if there's any  missing portfolio
                 if [rs2[0][pncol] for pncol in pncols] != list(pns):
                     raise ValueError('missing portfolio no. %s %s in a%s a ' %
                                      (list(pns),[rs2[0][pncol] for pncol in pncols], rs2[0][self.dcol]))
